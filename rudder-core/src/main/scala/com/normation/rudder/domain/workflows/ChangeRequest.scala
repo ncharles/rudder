f1c12efe99049c5ff80a96afa448f758f0b520e0
Vincent Membré <vincent.membre@normation.com>
2013-03-07 18:42:28 +0100

    Change Administration menu, add Change request page, try url changes

--- rudder-core/src/main/scala/com/normation/rudder/domain/workflows/ChangeRequest.scala
+++ rudder-core/src/main/scala/com/normation/rudder/domain/workflows/ChangeRequest.scala
@@ -51,8 +51,12 @@ import com.normation.rudder.domain.policies.DirectiveId
  */
 
 
-case class ChangeRequestId(value:String)
-
+case class ChangeRequestId(value:String) {
+  override def toString = value
+}
+object ChangeRequestId {
+  implicit def displayCRId (CRId:ChangeRequestId):String= CRId.value
+}
 
 sealed trait ChangeRequest {
         def id     : ChangeRequestId //modification Id ?
